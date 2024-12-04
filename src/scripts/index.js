6d47ce72a5117708ddc09a73decceea12159fda3
Jonathan Brandt <94417743+Jonathan-Brandt@users.noreply.github.com>
2024-12-03 19:02:37 -0600

    Add files via upload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,5 @@
 import "../pages/index.css";
 
-// import all the classes
 import FormValidator from "../scripts/FormValidator.js";
 import Card from "../scripts/Card.js";
 import Section from "../scripts/Section.js";
@@ -20,27 +19,36 @@ const cardSection = new Section(
     items: initialCards,
     renderer: (item) => {
       const card = new Card(item, "#card-template");
-      const cardElement = card.generateCard();
+      const cardElement = card.getView();
       cardSection.addItem(cardElement);
     },
   },
-  ".elements__list"
+  ".cards__list"
 );
 
 const popupWithImage = new PopupWithImage(".modal__image");
 popupWithImage.setEventListeners();
 
-const popupWithForm = new PopupWithForm(".modal__form", (formData) => {
+const popupWithForm = new PopupWithForm("#profile-edit-modal", (formData) => {
   userInfo.setUserInfo(formData);
 });
 popupWithForm.setEventListeners();
 
-const profileEditFormValidator = new FormValidator("#profile-edit-form");
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+const modalEditForm = document.querySelector("#modal-edit-form");
+const profileEditFormValidator = new FormValidator(config, modalEditForm);
 profileEditFormValidator.enableValidation();
 
-const addCardFormValidator = new FormValidator("#add-card-form");
+const modalAddForm = document.querySelector("#add-card-form");
+const addCardFormValidator = new FormValidator(config, modalAddForm);
 addCardFormValidator.enableValidation();
-
-formValidator.enableValidation();
 
 cardSection.renderItems();
